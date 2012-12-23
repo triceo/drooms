e8264d1a50124714d9113e420fbdc6d4cfaf420c
Petr Siroky <psiroky@redhat.com>
2012-12-23 16:27:01 +0100

    Playground centered and correctly resizeable

--- drooms-swing-gui/src/main/scala/org/drooms/gui/swing/DroomsSwingApp.scala
+++ drooms-swing-gui/src/main/scala/org/drooms/gui/swing/DroomsSwingApp.scala
@@ -9,6 +9,11 @@ import scala.swing.Orientation
 import scala.swing.event.ButtonClicked
 import scala.swing.Table
 import javax.swing.UIManager
+import scala.swing.MenuBar
+import scala.swing.Menu
+import scala.swing.MenuItem
+import scala.swing.BorderPanel
+import scala.swing.BoxPanel
 
 object DroomsSwingApp extends SimpleSwingApplication {
 
@@ -19,10 +24,17 @@ object DroomsSwingApp extends SimpleSwingApplication {
     menuBar = new MainMenu()
 
     val btn = new Button("Test")
-    val playground = new Playground()
-    val playersList = new PlayersList(List("Player 1", "Player 2"))
+    // left pane - playground + game controls
+    val leftPane = new BoxPanel(Orientation.Horizontal) {
+      val playground = new Playground()
+      import BorderPanel.Position._
+      //      layout(playground) = Center
+      //      layout(new Button("Control Panel")) = South
+      contents += playground
+    }
+    val rightPane = new PlayersList(List("Player 1", "Player 2"))
 
-    contents = new SplitPane(Orientation.Vertical, playground, playersList) {
+    contents = new SplitPane(Orientation.Vertical, leftPane, rightPane) {
       resizeWeight = 1.0
       rightComponent.minimumSize = new Dimension(150, 500)
       leftComponent.minimumSize = new Dimension(500, 500)
@@ -31,9 +43,25 @@ object DroomsSwingApp extends SimpleSwingApplication {
     listenTo(btn)
     reactions += {
       case ButtonClicked(`btn`) =>
-        playersList.addPlayer("Another player")
+        rightPane.addPlayer("Another player")
     }
     centerOnScreen()
 
+  }
+}
+
+class MainMenu extends MenuBar {
+  contents += new Menu("File") {
+    contents += new MenuItem("Open game log...")
+  }
+  contents += new Menu("Game") {
+    contents += new MenuItem("Start")
+    contents += new MenuItem("Restart")
+  }
+  contents += new Menu("Players") {
+    contents += new MenuItem("Settings...")
+  }
+  contents += new Menu("Help") {
+    contents += new MenuItem("About Drooms")
   }
 }
--- drooms-swing-gui/src/main/scala/org/drooms/gui/swing/Playground.scala
+++ drooms-swing-gui/src/main/scala/org/drooms/gui/swing/Playground.scala
@@ -2,15 +2,26 @@ package org.drooms.gui.swing
 
 import scala.swing.ScrollPane
 import scala.swing.Table
+import java.awt.Dimension
+import scala.swing.GridBagPanel
+import scala.swing.BorderPanel
 
 class Playground extends ScrollPane {
-  val table = new Table(10, 10) {
+  preferredSize = new Dimension(300, 300)
+  //maximumSize = new Dimension(300, 300)
+
+  val table = new Table(80, 60) {
+    preferredSize = new Dimension(700, 600)
+    //maximumSize = new Dimension(40, 40)
     rowHeight = 10
     selection.intervalMode = Table.IntervalMode.Single
     selection.elementMode = Table.ElementMode.Cell
+    peer.setTableHeader(null)
     //co
   }
 
-  viewportView = table
+  viewportView = new GridBagPanel {
+    layout(table) = new Constraints
+  }
 
 }
