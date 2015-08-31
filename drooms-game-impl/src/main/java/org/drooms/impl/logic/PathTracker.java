ee1e3e3b6da2f557f44ae61a0b0a4128a61761bb
Lukáš Petrovický <lpetrovi@redhat.com>
2015-08-31 09:59:51 +0200

    PathTracker path finding no longer limited in # of intermediate nodes
    We can now use any number of intermediate nodes through which to look for paths. However, tread carefully, as this is an NP-complete problem and, as such, the execution time grows dramatically with additional nodes.
    
    Also, I added a bunch of test coverage. We know now that the path finding actually works.

--- drooms-game-impl/src/main/java/org/drooms/impl/logic/PathTracker.java
+++ drooms-game-impl/src/main/java/org/drooms/impl/logic/PathTracker.java
@@ -20,15 +20,74 @@ import java.util.*;
  */
 public class PathTracker {
 
+    protected static <V, E> List<E> getPath(final Graph<V, E> graph, final V start, final Set<V> otherNodeSet) {
+        return PathTracker.getPath(graph, start, otherNodeSet, new UnweightedShortestPath<>(graph));
+    }
+
+    // synchronize access to the shortest path algorithm, otherwise it breaks down horribly in parallel streams
+    private static synchronized <V, E> List<E> getPath(final Graph<V, E> graph, final ShortestPath<V, E>
+            shortestPathAlgorithm, final V start, final V end) {
+        return ShortestPathUtils.getPath(graph, shortestPathAlgorithm, start, end);
+    }
+
+    /**
+     * Finds the shortest path through given nodes. This method assumes that all nodes exist in the graph and will
+     * not function properly otherwise.
+     *
+     * @param graph                 Graph to look inside of.
+     * @param start                 The starting node for the path.
+     * @param otherNodeSet          All the nodes to visit.
+     * @param shortestPathAlgorithm The algorithm for constructing shortest path. Must be thread-safe.
+     * @param <V>                   Type of node in the graph.
+     * @param <E>                   Type of edge in the graph.
+     * @return List of edges on the path, or empty if no path.
+     */
+    private static <V, E> List<E> getPath(final Graph<V, E> graph, final V start, final Set<V> otherNodeSet,
+                                          final ShortestPath<V, E> shortestPathAlgorithm) {
+        if (start == null || otherNodeSet == null || otherNodeSet.size() == 0) {
+            throw new IllegalArgumentException("Please provide both a start node and a set of other nodes.");
+        }
+        /*
+         * a brute-force algorithm to recursively find the shortest path. this algorithm picks all the other nodes,
+         * one after another, and will call this algorithm again with the chosen node as the starting node and the
+         * remaining nodes as the other nodes. it will try to be smart and not include among the other nodes the
+         * nodes that it already went through earlier in the recursion.
+         */
+        final List<E> path = otherNodeSet.parallelStream().filter(node -> !node.equals(start)).map(newStart -> {
+                // path to this node
+                final List<E> totalPath = PathTracker.getPath(graph, shortestPathAlgorithm, start, newStart);
+                // if any of the other nodes are in the above path already, don't go through them again
+                final Set<V> nodesToTraverse = new LinkedHashSet<>(otherNodeSet);
+                totalPath.forEach(edge -> nodesToTraverse.removeAll(graph.getIncidentVertices(edge)));
+                if (nodesToTraverse.size() > 0) {
+                    // find the new path and merge it
+                    totalPath.addAll(PathTracker.getPath(graph, newStart, Collections.unmodifiableSet(nodesToTraverse),
+                            shortestPathAlgorithm));
+                }
+                return totalPath;
+            }).min((edges, edges2) -> {
+                final int size1 = edges.size();
+                final int size2 = edges2.size();
+                if (size1 > size2) {
+                    return 1;
+                } else if (size1 == size2) {
+                    return 0;
+                } else {
+                    return -1;
+                }
+            }).orElseGet(Collections::emptyList);
+        return Collections.unmodifiableList(path);
+    }
+
     /**
      * Create a clone of the original graph with certain nodes removed.
      *
-     * @param src Original graph.
+     * @param src         Original graph.
      * @param removeNodes Nodes to remove from the original graph.
      * @return Original graph, except all the edges with specified incident nodes are removed.
      */
     private static UndirectedGraph<Node, Edge> cloneGraph(final Graph<Node, Edge> src,
-            final Collection<Node> removeNodes) {
+                                                          final Collection<Node> removeNodes) {
         final UndirectedGraph<Node, Edge> clone = new UndirectedSparseGraph<>();
         src.getEdges().forEach(e -> {
             final boolean isEdgeAdded = clone.addEdge(e, src.getIncidentVertices(e));
@@ -49,11 +108,9 @@ public class PathTracker {
 
     /**
      * Initialize the class.
-     * 
-     * @param playground
-     *            The playground to base the path-finding algos on.
-     * @param p
-     *            The player that will be using this particular instance.
+     *
+     * @param playground The playground to base the path-finding algos on.
+     * @param p          The player that will be using this particular instance.
      */
     public PathTracker(final Playground playground, final Player p) {
         this.playground = playground;
@@ -63,54 +120,34 @@ public class PathTracker {
     /**
      * Retrieve the current position of the player's worm's head, that is the
      * one found during the last {@link #updatePlayerPositions(Map)} call.
-     * 
+     *
      * @return The position, or null if {@link #updatePlayerPositions(Map)} had never been
-     *         called before.
+     * called before.
      */
     public Node getCurrentPosition() {
         return this.currentPosition;
     }
 
-    /**
-     * Find the shortest path between two nodes.
-     * 
-     * @param start
-     *            Beginning of the path.
-     * @param end
-     *            End of the path.
-     * @return Unmodifiable list of nodes on the path, ordered from start to
-     *         end. Empty if path cannot be found.
-     */
-    public List<Edge> getPath(final Node start, final Node end) {
-        return Collections.unmodifiableList(ShortestPathUtils.getPath(this.currentGraph, this.currentPath, start, end));
+    protected List<Edge> getPath(final Node start, final Set<Node> otherNodeSet) {
+        if (!this.currentGraph.containsVertex(start)) {
+            throw new IllegalArgumentException("Starting node not in the graph.");
+        } else if (otherNodeSet.stream().filter(node -> this.currentGraph.containsVertex(node)).count() > 0) {
+            throw new IllegalArgumentException("Some of the other nodes are not in the graph.");
+        }
+        return PathTracker.getPath(this.currentGraph, start, otherNodeSet, this.currentPath);
     }
 
     /**
-     * Find the shortest path from the start node that leads through the other
-     * two nodes in any order.
-     * 
-     * @param start
-     *            Beginning of the path.
-     * @param node2
-     *            Any of the other two nodes.
-     * @param node3
-     *            Any of the other two nodes.
-     * @return Unmodifiable list of nodes on the path, ordered from start to
-     *         end. Empty if path cannot be found.
+     * Find the shortest path from the start node that leads through the other nodes regardless of their order.
+     * <p>
+     * This is effectively TSP, so try to keep the amount of nodes very, very small. :-)
+     *
+     * @param start      Beginning of the path.
+     * @param otherNodes All the other nodes to go through. If this includes the start node, it will be ignored.
+     * @return Unmodifiable list of nodes on the path, ordered from start to end. Empty if path cannot be found.
      */
-    public List<Edge> getPath(final Node start, final Node node2, final Node node3) {
-        final List<Edge> path2 = this.getPath(start, node2);
-        final List<Edge> path3 = this.getPath(start, node3);
-        final List<Edge> path23 = new ArrayList<Edge>(this.getPath(node2, node3));
-        final List<Edge> result = new ArrayList<Edge>();
-        if (path2.size() > path3.size()) {
-            result.addAll(path3);
-            Collections.reverse(path23);
-        } else {
-            result.addAll(path2);
-        }
-        result.addAll(path23);
-        return Collections.unmodifiableList(result);
+    public List<Edge> getPath(final Node start, final Node... otherNodes) {
+        return this.getPath(start, Collections.unmodifiableSet(new LinkedHashSet<>(Arrays.asList(otherNodes))));
     }
 
     public Player getPlayer() {
@@ -124,16 +161,15 @@ public class PathTracker {
     /**
      * Update the internal state of this class so that future paths can avoid
      * places where the worms currently reside.
-     * 
-     * @param newPositions
-     *            New current positions of all the worms.
+     *
+     * @param newPositions New current positions of all the worms.
      */
     protected void updatePlayerPositions(final Map<Player, Deque<Node>> newPositions) {
         if (newPositions.isEmpty()) {
             this.currentGraph = Graphs.unmodifiableGraph(this.playground.getGraph());
         } else {
             // enumerate all the nodes occupied by worms at this point
-            final Set<Node> unavailable = new HashSet<>();
+            final Set<Node> unavailable = new LinkedHashSet<>();
             newPositions.forEach((player, nodes) -> unavailable.addAll(nodes));
             // make sure we keep the head node, since otherwise there is no path from the current position to any other
             this.currentPosition = newPositions.get(this.player).getFirst();
---
+++ drooms-game-impl/src/test/java/org/drooms/impl/logic/PathTrackerTest.java
@@ -0,0 +1,96 @@
+package org.drooms.impl.logic;
+
+import edu.uci.ics.jung.graph.Graph;
+import org.drooms.api.Edge;
+import org.drooms.api.Node;
+import org.drooms.api.Playground;
+import org.drooms.impl.DefaultGame;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
+
+public class PathTrackerTest {
+
+    private static final Playground PLAYGROUND = new DefaultGame().buildPlayground("test", PathTrackerTest.class
+            .getResourceAsStream("testing.playground"));
+    private static final Graph<Node, Edge> GRAPH = PathTrackerTest.PLAYGROUND.getGraph();
+
+    @Test
+    public void testPathToItself() {
+        final Node start = PLAYGROUND.getNodeAt(10, 10);
+        Assert.assertEquals(Collections.EMPTY_LIST, PathTracker.getPath(GRAPH, start, Collections.singleton(start)));
+    }
+
+    @Test
+    public void testPathToNext() {
+        final Node start = PLAYGROUND.getNodeAt(10, 10);
+        final Node end = PLAYGROUND.getNodeAt(10, start.getY() + 1);
+        final List<Edge> path = PathTracker.getPath(GRAPH, start, Collections.singleton(end));
+        Assert.assertEquals(1, path.size());
+        final Edge edge = path.get(0);
+        Assert.assertEquals(start, edge.getNodes().getLeft());
+        Assert.assertEquals(end, edge.getNodes().getRight());
+    }
+
+    private static int getManhattanDistance(final Node start, final Node end) {
+        return Math.abs(end.getX() - start.getX()) + Math.abs(end.getY() - start.getY());
+    }
+
+    private static Set<Node> toSet(final Node... nodes) {
+        return new LinkedHashSet<>(Arrays.asList(nodes));
+    }
+
+    @Test
+    public void testShortestPath() {
+        final Node start = PLAYGROUND.getNodeAt(10, 10);
+        final Node end = PLAYGROUND.getNodeAt(15, 15);
+        // the length of shortest path between two objects in a 2D grid is their Manhattan distance
+        final int manhattanDistance = PathTrackerTest.getManhattanDistance(start, end);
+        final List<Edge> path = PathTracker.getPath(GRAPH, start, Collections.singleton(end));
+        Assert.assertEquals(manhattanDistance, path.size());
+    }
+
+    private void testShortestPathThroughNode(final boolean endFirst) {
+        // pass through a corner node
+        final Node start = PLAYGROUND.getNodeAt(3, 0);
+        final Node middle = PLAYGROUND.getNodeAt(0, 0);
+        final Node end = PLAYGROUND.getNodeAt(0, 3);
+        // the length of shortest path between two objects in a 2D grid is their Manhattan distance
+        final int manhattanDistance = PathTrackerTest.getManhattanDistance(start, end);
+        final List<Edge> path = PathTracker.getPath(GRAPH, start, endFirst ? PathTrackerTest.toSet(end, middle) :
+                PathTrackerTest.toSet(middle, end));
+        Assert.assertEquals(manhattanDistance, path.size());
+        // make sure that in the path there is the middle node
+        final Collection<Edge> edges = GRAPH.getIncidentEdges(middle);
+        Assert.assertEquals(2, edges.size()); // otherwise the following assertion makes no sense
+        edges.forEach(edge -> Assert.assertTrue(path.contains(edge)));
+    }
+
+    @Test
+    public void testShortestPathThroughNodeMiddleFirst() {
+        this.testShortestPathThroughNode(false);
+    }
+
+    @Test
+    public void testShortestPathThroughNodeEndFirst() {
+        this.testShortestPathThroughNode(true);
+    }
+
+    @Test
+    public void testShortestPathThroughTwoNodes() {
+        final int DISTANCE = 3;
+        // pass through a corner node
+        final Node start = PLAYGROUND.getNodeAt(DISTANCE, 0);
+        final Node middle1 = PLAYGROUND.getNodeAt(0, 0);
+        final Node middle2 = PLAYGROUND.getNodeAt(DISTANCE, DISTANCE);
+        final Node end = PLAYGROUND.getNodeAt(0, DISTANCE);
+        // minimal possible path length
+        final List<Edge> path = PathTracker.getPath(GRAPH, start, PathTrackerTest.toSet(middle1, end, middle2));
+        Assert.assertEquals(DISTANCE + DISTANCE + DISTANCE, path.size());
+        // make sure that all nodes are in path
+        Set<Node> nodesToFind = PathTrackerTest.toSet(start, middle1, middle2, end);
+        path.forEach(edge -> nodesToFind.removeAll(GRAPH.getIncidentVertices(edge)));
+        Assert.assertEquals(0, nodesToFind.size());
+    }
+}
